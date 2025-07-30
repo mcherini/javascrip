c692e12ac7014a57e7275fbf557ded61d78d5a9d
Mauro Cherini <maurocherini@gmail.com>
2025-07-30 08:49:29 -0300

    1 curso

---
+++ main.js
@@ -0,0 +1,20 @@
+// console.log("funciona");
+// prompt("ingrese el numero");
+// alert("cuidado aiuda");
+//  let y const son variables
+// el iniciador, el nombre = el valor asignado
+// = es de asignacion no de igualdad
+//datos booleanos= verificamos si o no, true o false
+
+let nombre = prompt("ingrese el nombre");
+let emailDelUsuario = prompt("ingrese el mail");
+let edad = prompt("ingrese su edad");
+console.log(nombre);
+console.log(emailDelUsuario);
+console.log(edad);
+
+//contatenar
+let contatenar = "hola " + nombre + " como estas?";
+console.log(contatenar);
+let contatenar2 = `hola ${nombre} como estás?`;
+console.log(contatenar2);
